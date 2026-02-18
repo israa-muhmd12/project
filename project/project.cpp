1e1f85856bf739f30944704a4c0204ba858ebb35
israa-muhmd12 <israamuhmd97@gmail.com>
2026-02-18 19:55:36 +0200

    Add Employee class with attributes and methods

--- project/project.cpp
+++ project/project.cpp
@@ -72,3 +72,72 @@ void display() {
     cout << "Balance: " << balance << endl;
 }
 };
+class Employee {
+
+private:
+    int id;
+    string name;
+    string password;
+    double salary;
+
+public:
+
+    void setId(int id) {
+        this->id = id;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    void setName(string name) {
+
+        if (name.size() >= 5 && name.size() <= 20) {
+
+            bool valid = true;
+
+            for (int i = 0; i < name.size(); i++) {
+                if (!isalpha(name[i])) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                this->name = name;
+            }
+        }
+    }
+
+    string getName() {
+        return name;
+    }
+
+    void setPassword(string password) {
+
+        if (password.size() >= 8 && password.size() <= 20) {
+            this->password = password;
+        }
+    }
+
+    string getPassword() {
+        return password;
+    }
+
+    void setSalary(double salary) {
+
+        if (salary >= 5000) {
+            this->salary = salary;
+        }
+    }
+
+    double getSalary() {
+        return salary;
+    }
+
+    void display() {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Salary: " << salary << endl;
+    }
+};
